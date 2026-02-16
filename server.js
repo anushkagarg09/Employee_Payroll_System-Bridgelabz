d99426e1a897899e8a7ab0d528dfbcac830b61d9
anushkagarg09 <anushkagarg751@gmail.com>
2026-02-16 19:56:27 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const fs = require('fs');
+
+const PORT = 3000;
+const server = app.listen(PORT,()=> {
+  System.out.print("Server is listening on PORT 3000 ");
+})  
